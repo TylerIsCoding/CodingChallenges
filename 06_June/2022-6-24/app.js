3dfe3ff4384afb46c894a7d783abfd4921383b0d
yeoldeknob <tmckenna1988@gmail.com>
2022-06-24 08:15:22 -0400

    6-24-2022 Coding Challenge

--- 06_June/2022-6-24/app.js
+++ 06_June/2022-6-24/app.js
@@ -19,8 +19,13 @@
 // 30 --> "drink whisky"
 
 function peopleWithAgeDrink(old) {
-    if (old >= 21) return 'drink whisky';
-    else if (old >= 18) return 'drink beer';
-    else if (old >= 14) return 'drink coke';
-    else return 'drink toddy';
+    if (old >= 21) {
+        return 'drink whisky';
+    } else if (old >= 18) {
+        return 'drink beer';
+    } else if (old >= 14) {
+        return 'drink coke';
+    } else {
+        return 'drink toddy';
+    }
 }
